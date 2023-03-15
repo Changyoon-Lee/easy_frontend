62444f4284786a34e36b714e4c5d543aff98a253
Changyoon-Lee <lkingkongl@naver.com>
2023-03-15 10:05:57 +0900

    fix aws middleware

--- src/middlewares.ts
+++ src/middlewares.ts
@@ -1,9 +1,10 @@
 import { Request, Response, NextFunction } from "express";
 import multer from "multer";
 import multerS3 from "multer-s3";
-import aws from "@aws-sdk/client-s3"
+import { S3Client } from "@aws-sdk/client-s3"
 
-const s3 = new aws.S3({
+const s3 = new S3Client({
+    region:"ap-northeast-2",
     credentials: {
         accessKeyId: process.env.AWS_ID,
         secretAccessKey: process.env.AWS_SECRET,
